21c546e5fbf33f65db468d313b5fe5afe510a791
Jeet Das <53969369+2019jeetdas@users.noreply.github.com>
2022-04-05 20:27:34 +0530

    Update form-to-email.php

--- form-to-email.php
+++ form-to-email.php
@@ -2,9 +2,4 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-  
-  $to = "jeetdas32@gmail.com";
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $visitor_email \r\n";
-  mail($to,$email_subject,$email_body,$headers);
 ?>
